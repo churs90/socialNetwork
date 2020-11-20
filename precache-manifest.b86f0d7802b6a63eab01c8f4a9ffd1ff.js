a0a88560fb405f7616aeaf79ca81c165b0705fa6
Vadim Chursin <vadchurs@mail.ru>
2020-11-20 19:16:36 +0300

    Updates

---
+++ precache-manifest.b86f0d7802b6a63eab01c8f4a9ffd1ff.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91184c2c5596bd40a4a7061a6721023",
+    "url": "/socialNetwork/index.html"
+  },
+  {
+    "revision": "ebd9cca76b187dffb0ef",
+    "url": "/socialNetwork/static/css/main.6c0412a3.chunk.css"
+  },
+  {
+    "revision": "e359ec772c3abe37f882",
+    "url": "/socialNetwork/static/js/2.f20b9706.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/socialNetwork/static/js/2.f20b9706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebd9cca76b187dffb0ef",
+    "url": "/socialNetwork/static/js/main.bef0de71.chunk.js"
+  },
+  {
+    "revision": "8030d259acf21e3792a6",
+    "url": "/socialNetwork/static/js/runtime-main.f4a284c9.js"
+  },
+  {
+    "revision": "d178c0d912081ef50051b6865b86a733",
+    "url": "/socialNetwork/static/media/logo-mortal-kombat.d178c0d9.png"
+  },
+  {
+    "revision": "a6486ffcceabe12fbd7a93d355c261af",
+    "url": "/socialNetwork/static/media/preloader.a6486ffc.gif"
+  },
+  {
+    "revision": "9775c4b8c6c465fe4044cf7b0408d05b",
+    "url": "/socialNetwork/static/media/user.9775c4b8.jpg"
+  }
+]);
